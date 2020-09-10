f98dfa8971571ac145ce3dad472a398ae6dfa6f3
Emil Manafov <mnf.emil@gmail.com>
2020-09-10 23:08:53 +0400

    Added new migration

---
+++ migrations/Version20200910190815.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200910190815 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX uniq_99165a59433b78c4');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('CREATE UNIQUE INDEX uniq_99165a59433b78c4 ON vacancies (vacancy_id)');
+    }
+}
