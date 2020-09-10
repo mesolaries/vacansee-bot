814af32a31101bd886a592642ea34c68b0334468
Emil Manafov <mnf.emil@gmail.com>
2020-09-10 23:07:52 +0400

    Fixed bug with unique constraint

--- src/Entity/Vacancy.php
+++ src/Entity/Vacancy.php
@@ -20,7 +20,7 @@ class Vacancy
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255, unique=true)
+     * @ORM\Column(type="string", length=255)
      */
     private $vacancyId;
 
